75df92c25b325e443ab53c809004c576a6321f79
neelanshdubey <neelansh.dubey3253@gmail.com>
2026-08-11 16:02:43 +0530

    day-2-rich-quote

--- Day -1/AI-Strategy-Task/OrderApi.Tests/obj/Debug/net10.0/OrderApi.Tests.AssemblyInfo.cs
+++ Day -1/AI-Strategy-Task/OrderApi.Tests/obj/Debug/net10.0/OrderApi.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderApi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72a8e6fb6a3f12438ae0bb27fc273334bcab4677")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a0240b8e953584681d1c42cc5f889aab059ca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderApi.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderApi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
